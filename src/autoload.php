f9b0c26c45daacc5f068c261b8b5930328ed4843
DuetG <1274703+duetg@users.noreply.github.com>
2026-04-13 13:31:52 +0800

    Remove global duetgaicon_debug wrapper function

--- src/autoload.php
+++ src/autoload.php
@@ -10,20 +10,6 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-/**
- * Debug logging helper - wraps WordPress\CustomAiProvider\Helper::debug()
- *
- * @param string $message The message to log
- * @param mixed $data Optional data to include (lazy JSON encoding, only when debug is enabled)
- * @param int $maxDataLength Maximum length for data serialization (0 = no limit)
- */
-if (!function_exists('duetgaicon_debug')) {
-    function duetgaicon_debug($message, $data = null, $maxDataLength = 0)
-    {
-        Helper::debug($message, $data, $maxDataLength);
-    }
-}
-
 spl_autoload_register(function ($class) {
     $prefix = 'WordPress\\CustomAiProvider\\';
     $base_dir = __DIR__ . '/';
